5b3acfd643385f09e3af9c804acc858f6b001e1b
readybuilderone <neohan2016@outlook.com>
2021-08-05 11:18:35 +0800

    added data generator sourcecode

---
+++ generator-sourcecode/set_variables.sql
@@ -0,0 +1,7 @@
+set @@global.tidb_distsql_scan_concurrency = 100;
+set @@global.tidb_index_lookup_concurrency = 50;
+set @@global.tidb_hash_join_concurrency = 20;
+set @@global.tidb_index_serial_scan_concurrency = 10;
+set @@global.tidb_projection_concurrency = 10;
+set @@global.tidb_hashagg_partial_concurrency = 10;
+set @@global.tidb_hashagg_final_concurrency = 10;
